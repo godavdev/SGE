f94e23dd3d47395d2b97f2e46f5554c3f8d25ba9
GonAvi <dgaduff@gmail.com>
2023-10-03 20:52:09 -0600

    Se refino el login

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useAuth } from '../context/authContext'
+import { useNavigate } from 'react-router-dom'
 
 const Dashboard = () => {
+
+    const { loading, user, logOut } = useAuth()
+
+    const navigate = useNavigate()
+
+    if (loading) return <div>Cargando...</div>
+
+    const handleLogOut = async () => {
+        await logOut()
+        navigate("/login")
+    }
+
     return (
-        <div>Dashboard</div>
+        <div>
+            {user.email}
+            <button onClick={handleLogOut}>Cerrar sesion</button>
+        </div>
     )
 }
 
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuth } from '../context/authContext'
+import { useNavigate } from 'react-router-dom'
+
 
 const Login = () => {
+
+    const { logIn } = useAuth()
+
+    const navigate = useNavigate()
+
+    const [user, setUser] = useState(null)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await logIn(user.email, user.password)
+            navigate("/")
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
+    const handleUserChange = ({ target: { name, value } }) => {
+        setUser({ ...user, [name]: value })
+        console.log(user)
+    }
+
     return (
-        <div>Login</div>
+        <>
+            {/* Insertar componente de form aqui, tiene que tener la propiedad onChange y onSubmit */}
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">Correo electronico:</label>
+                <input type="email" name='email' id='email' placeholder='Correo electronico' onChange={handleUserChange} />
+                <label htmlFor="password">Contrasena:</label>
+                <input type="password" name="password" id="password" placeholder='Contrasena' onChange={handleUserChange} />
+                <button type="submit">Iniciar sesion</button>
+            </form>
+        </>
     )
 }
 
